8cc9be87a62bf4491535cd55801d41c7ee8883f5
OneDevMan <enderfight95@gmail.com>
2023-10-16 01:02:08 +0200

    update

--- dist/shared/utils/node/dnsz/getdnsz.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getdnsz = void 0;
-;
-// 
-function getdnsz() {
-    return Object.getPrototypeOf(async function () { }).constructor(`return await import("dnsz");`)();
-}
-exports.getdnsz = getdnsz;
